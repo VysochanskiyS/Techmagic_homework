ca0d92ded1816f1b00ae69a55149bee1f6a6025d
VysochanskiyS <62776197+VysochanskiyS@users.noreply.github.com>
2021-11-11 13:53:27 +0200

    Delete workshop-mongoose-part4 directory

--- workshop-mongoose-part4/models/user.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const UserSchema = new Schema({});
-
-module.exports = mongoose.model('User', UserSchema);
--- workshop-mongoose-part4/routes/index.js
+++
@@ -1,9 +0,0 @@
-const userRoutes = require('./user.route');
-const articleRoutes = require('./article.route');
-const express = require('express');
-const router = express.Router();
-
-router.use('/user', userRoutes);
-router.use('/article', articleRoutes);
-
-module.exports = router;
